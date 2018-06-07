f7e80ee9ef4f1c334297187fb532023d957ccac4
ieee0824 <ieee0824x@gmail.com>
2018-06-07 13:13:37 +0900

    uptime

--- plugins/slack/slack.go
+++ plugins/slack/slack.go
@@ -3,10 +3,12 @@ package slack
 import (
 	"encoding/json"
 	"flag"
-	"image/color"
+	"fmt"
+	"reflect"
 
 	"github.com/ieee0824/getenv"
 	"github.com/ieee0824/sakuya"
+	"github.com/ieee0824/skyclad/client/io"
 	"github.com/ieee0824/skyclad/notifer"
 )
 
@@ -16,12 +18,17 @@ func init() {
 
 type Slack struct {
 	APIURL   *string
-	Username string
+	Username *string
+	Channel  *string
+	Title    *string
 }
 
 func New() *Slack {
 	return &Slack{
-		APIURL: flag.String("slack-api", getenv.String("SLACK_INCOMING_API_URL"), "slack incoming url. \"SLACK_INCOMING_API_URL\" may be specified as an environment variable."),
+		APIURL:   flag.String("slack-api", getenv.String("SLACK_INCOMING_API_URL"), "slack incoming url. \"SLACK_INCOMING_API_URL\" may be specified as an environment variable."),
+		Username: flag.String("slack-username", getenv.String("SLACK_USERNAME"), "slack username."),
+		Channel:  flag.String("slack-channel", "", "slack channel"),
+		Title:    flag.String("slack-title", "", "slack message title"),
 	}
 }
 
@@ -29,19 +36,28 @@ func (s *Slack) SetEncoder(e notifer.Encoder) {
 }
 
 func (s *Slack) Notice(v interface{}) error {
-	w := sakuya.NewIncomingWriter(*s.APIURL, s.Username)
-	w.SetBaseColor(color.RGBA{
-		0xff,
-		0x00,
-		0x00,
-		0xff,
-	})
-	bin, err := json.Marshal(v)
-	if err != nil {
-		return err
+	w := sakuya.NewIncomingWriter(*s.APIURL, *s.Username)
+	switch v := v.(type) {
+	case []clientio.GetContainerOutput:
+		w.AddTitle(*s.Title)
+		for _, o := range v {
+			bin, err := json.MarshalIndent(o, "", "  ")
+			if err != nil {
+				continue
+			}
+			w.AddInfo(string(bin))
+		}
+
+		return w.Flush()
+	default:
+		fmt.Println(reflect.TypeOf(v))
+		bin, err := json.Marshal(v)
+		if err != nil {
+			return err
+		}
+		if _, err := w.Write(bin); err != nil {
+			return err
+		}
+		return nil
 	}
-	if _, err := w.Write(bin); err != nil {
-		return err
-	}
-	return nil
 }
