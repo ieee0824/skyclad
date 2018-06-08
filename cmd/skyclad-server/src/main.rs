1e66e31825ebf20eec6dc7d9e78fe85cd473e4ec
ieee0824 <ieee0824x@gmail.com>
2018-06-08 16:38:02 +0900

    template

--- cmd/skyclad-server/src/main.rs
+++ cmd/skyclad-server/src/main.rs
@@ -1,3 +1,14 @@
+#[macro_use] extern crate nickel;
+use nickel::{Nickel, HttpRouter};
+
 fn main() {
-    println!("Hello, world!");
+    let mut serv = Nickel::new();
+
+    serv.get("/bar", middleware!("This is the /bar handler"));
+    serv.get("/user/:userid", middleware! { |request|
+      format!("<h1>This is user: {:?}</h1>", request.param("userid").unwrap())
+    });
+    serv.get("/a/*/d", middleware!("matches /a/b/d but not /a/b/c/d"));
+    serv.get("/a/**/d", middleware!("This matches /a/b/d and also /a/b/c/d"));
+    serv.listen("127.0.0.1:6767");
 }
